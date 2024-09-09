de9e6a4dc11276cf9434e9d85f48095cd8f162ac
alessandrovarc-git <alessandro.varchetta1996@gmail.com-git>
2024-09-09 16:36:14 +0200

    card-body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
+import CardProfile from './components/CardProfile';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-   <>
-   ciao
-   </>
+  <CardProfile/>
   );
 }
 
