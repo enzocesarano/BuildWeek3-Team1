df501e1d0ad43409fb5dfd4fedf783d851883928
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-09-10 11:35:00 +0200

    EliminatiFootereSidebardallaHome

--- src/App.js
+++ src/App.js
@@ -8,30 +8,38 @@ import { Col, Container, Row } from "react-bootstrap";
 import LinkedInSidebar from "./components/Sidebar";
 import MyFooter from "./components/MyFooter";
 import NavScroll from "./components/MyNav";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <div className="bg-secondary-subtle">
-          <NavScroll />
-          <Container>
-            <Row className="py-5">
-              <Col className="col-12 col-md-8 mt-5">
-                <Routes>
-                  <Route path="/" element={<ProfileArea />} />
-                  <Route path="/home" element={<Home />} />
-                </Routes>
-              </Col>
-              <LinkedInSidebar />
-            </Row>
-          </Container>
-          <MyFooter />
-        </div>
+        <AppContent />
       </BrowserRouter>
     </Provider>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <div className="bg-secondary-subtle">
+      <NavScroll />
+      <Container>
+        <Row className="py-5">
+          <Col className="col-12 col-md-8 mt-5">
+            <Routes>
+              <Route path="/" element={<ProfileArea />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </Col>
+          {location.pathname !== "/home" && <LinkedInSidebar />}
+        </Row>
+      </Container>
+      {location.pathname !== "/home" && <MyFooter />}
+    </div>
   );
 }
 
