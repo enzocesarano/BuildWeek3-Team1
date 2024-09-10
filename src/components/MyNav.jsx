df815d2da1c454bce7eea578b7e7ae01ab8feb04
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-09-10 18:25:04 +0200

    up

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -9,14 +9,23 @@ import {
   FaBell,
   FaTag,
   FaStar,
+  FaPlus,
+  FaBullseye,
+  FaPlay,
+  FaInfoCircle,
+  FaChartLine,
+  FaUsers,
+  FaCompass,
+  FaTh,
 } from "react-icons/fa";
 import { BsChatDotsFill } from "react-icons/bs";
 import SearchModal from "./SearchModal";
 
 function NavScroll() {
   const myProfile = useSelector((state) => state.myProfile.myProfile);
-  const searchProfile = useSelector((state) => state.searchProfile.searchProfile)
-  console.log(searchProfile)
+  const searchProfile = useSelector(
+    (state) => state.searchProfile.searchProfile
+  );
   const arrayAllProfiles = useSelector(
     (state) => state.arrayAllProfiles.arrayAllProfiles
   );
@@ -24,9 +33,8 @@ function NavScroll() {
   const [value, setValue] = useState("");
   const [modalShow, setModalShow] = useState(false);
 
-
   const handleProfileSelect = () => {
-    setValue(""); 
+    setValue("");
     setModalShow(false);
   };
 
@@ -161,6 +169,170 @@ function NavScroll() {
                   <NavDropdown.Item href="#" className="text-secondary">
                     Esci
                   </NavDropdown.Item>
+                </NavDropdown>
+              </div>
+              <div className="icon-above-dropdown">
+                <a href="#">
+                  <div className="icon-with-text">
+                    <FaTh className="icon workIconColor" />
+                  </div>
+                </a>
+
+                <NavDropdown
+                  title="Per le aziende"
+                  id="navbarScrollingDropdown"
+                  align="end"
+                >
+                  <div className="dropdown-columns mt-5">
+                    <div className="column">
+                      <p>
+                        <strong>Scopri altri prodotti LinkedIn</strong>
+                      </p>
+                      <a href="#" className="mb-4 mt-5 fw-bold ms-3 ">
+                        <FaCompass className="icon iconBlu me-3  " />{" "}
+                        <span className="DropdownColortext small-text">
+                          {" "}
+                          Trova lead{" "}
+                        </span>
+                      </a>
+                      <a href="#" className="mb-5 mt-3 fw-bold ms-3 ">
+                        <FaUsers className="icon iconBlu me-3 " />{" "}
+                        <span className="DropdownColortext small-text">
+                          {" "}
+                          Gruppi{" "}
+                        </span>
+                      </a>
+                      <a href="#" className="text-secondary ">
+                        Talent
+                      </a>
+                      <a href="#" className="mb-5 mt-3 fw-bold ms-3 ">
+                        <FaChartLine className="icon iconBlu me-3 " />
+                        <span className="DropdownColortext small-text">
+                          {" "}
+                          Talent Insights
+                        </span>
+                      </a>
+                      <a href="#" className="text-secondary ">
+                        Vendite
+                      </a>
+                      <a href="#" className="mb-5 mt-3 fw-bold ms-3 ">
+                        <FaInfoCircle className="icon iconBlu me-3 " />
+                        <span className="DropdownColortext small-text">
+                          {" "}
+                          Marketplace dei servizi
+                        </span>
+                      </a>
+                      <a href="#" className="text-secondary ">
+                        Marketing
+                      </a>
+                      <a href="#" className="mb-5 mt-3 fw-bold ms-3 ">
+                        <FaBullseye className="icon iconBlu me-3 " />
+                        <span className="DropdownColortext small-text">
+                          {" "}
+                          Pubblicizza
+                        </span>
+                      </a>
+                      <a href="#" className="text-secondary ">
+                        Learning
+                      </a>
+                      <a href="#" className="mb-5 mt-3 fw-bold ms-3 ">
+                        <FaPlay className="icon iconBlu me-3 " />{" "}
+                        <span className="DropdownColortext small-text">
+                          Learning
+                        </span>
+                      </a>
+                    </div>
+                    <div className="column">
+                      <p>
+                        <strong className="ms-3">
+                          Scopri altro per il business
+                        </strong>
+                      </p>
+                      <div>
+                        <a href="#">
+                          <div className="mt-4">
+                            <strong className="fw-bold ms-3 DropdownColortext small-text">
+                              Assumi su LinkedIn
+                            </strong>
+                            <p className="ms-3 secSmalltext ">
+                              Trova, attrai e assumi
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                      <div>
+                        <a href="#">
+                          <div className="mt-4">
+                            <strong className="fw-bold ms-3 DropdownColortext small-text">
+                              Vendi con LinkedIn
+                            </strong>
+                            <p className="ms-3 secSmalltext">
+                              Sblocca nuove opportunità di vendita
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                      <div className="mt-4">
+                        <a href="#">
+                          <div>
+                            <strong className="fw-bold ms-3 DropdownColortext small-text">
+                              Offerta di lavoro gratuita
+                            </strong>
+                            <p className="ms-3 secSmalltext">
+                              Ottieni rapidamente candidati qualificati
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                      <div className="mt-4">
+                        <a href="#">
+                          <div>
+                            <strong className="fw-bold ms-3 DropdownColortext small-text">
+                              Fai pubblicità su LinkedIn
+                            </strong>
+                            <p className="ms-3 secSmalltext">
+                              Acquisisci clienti e fai crescere la tua azienda
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                      <div className="mt-4">
+                        <a href="#">
+                          <div>
+                            <strong className="fw-bold ms-3 DropdownColortext small-text">
+                              Impara con LinkedIn
+                            </strong>
+                            <p className="ms-3 secSmalltext">
+                              Assumi su LinkedIn
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                      <div className="mb-3 mt-4">
+                        <a href="#">
+                          <div>
+                            <strong className="fw-bold ms-3 DropdownColortext small-text">
+                              Admin Center
+                            </strong>
+                            <p className="ms-3 secSmalltext">
+                              Gestisci i dettagli di fatturazione e account
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                      <a href="#">
+                        <div>
+                          <p>
+                            <strong className="ms-3 DropdownColortext small-text">
+                              Crea una pagina aziendale
+                            </strong>
+
+                            <FaPlus className="icon ms-3 iconPlus " />
+                          </p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
                 </NavDropdown>
               </div>
               <div className="separator-bar"></div>
