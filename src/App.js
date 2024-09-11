49e291efbdd0ce157c333bc8e120a3f99b6f07e6
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-09-11 11:18:12 +0200

    SearchJob

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 import { getProfile } from "./action";
 import { useEffect } from "react";
+import SearchJob from "./components/SearchJob";
 
 function App() {
   return (
@@ -28,12 +29,12 @@ function AppContent() {
   const location = useLocation();
 
   const myProfile = useSelector((state) => state.myProfile.myProfile);
-  const searchProfile = useSelector((state) => state.searchProfile.searchProfile)
+  const searchProfile = useSelector((state) => state.searchProfile.searchProfile);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getProfile('me',''));
+    dispatch(getProfile("me", ""));
   }, []);
 
   return (
@@ -50,10 +51,15 @@ function AppContent() {
               <Col className="col-12 col-md-8 mt-5">
                 <Routes>
                   <Route path="/profile" element={<ProfileArea myProfile={myProfile} />} />
-                  <Route path="/profile/:id" element={<ProfileArea myProfile={searchProfile}/>} />
+                  <Route path="/profile/:id" element={<ProfileArea myProfile={searchProfile} />} />
+                  <Route path="/search-job" element={<SearchJob />} />
                 </Routes>
               </Col>
-              {location.pathname === "/profile" ? <LinkedInSidebar myProfile={myProfile}/> : <LinkedInSidebar myProfile={searchProfile}/>}
+              {location.pathname === "/profile" ? (
+                <LinkedInSidebar myProfile={myProfile} />
+              ) : (
+                <LinkedInSidebar myProfile={searchProfile} />
+              )}
             </>
           )}
 
